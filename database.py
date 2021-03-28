8fd6ea02494b27d4e60a5498f468dcc231a32741
Duc Minh Le <leminhducktvn@gmail.com>
2021-03-28 23:29:44 +0200

    [Chore] Add lock for database

--- database.py
+++ database.py
@@ -1,18 +1,26 @@
-import yaml
+import yaml, threading
 
 FILE = "database.yaml"
 
 storage = {}
+mutex = threading.Lock()
 
 def create(sid, name, value):
   global storage
+  global mutex
+
   print("[DATABASE] Creating SID %s - name %s" % (sid, name))
-  if sid not in storage:
-    storage[sid] = {}
-  if name in storage[sid]:
-    print("[DATABASE] Error - %s already exits in SID %s" % (name, sid))
-    return False
-  storage[sid][name] = value
+
+  mutex.acquire()
+  try:
+    if sid not in storage:
+      storage[sid] = {}
+    if name in storage[sid]:
+      print("[DATABASE] Error - %s already exits in SID %s" % (name, sid))
+      return False
+    storage[sid][name] = value
+  finally:
+    mutex.release()
   print("[DATABASE] Created - SID %s - name %s" % (sid, name))
   return True
 
@@ -25,32 +33,57 @@ def query(sid):
 
 def reset(sid):
   global storage
-  print("[DATABASE] Reset %s" % sid)
-  storage.pop(sid, None)
+  global mutex
+
+  mutex.acquire()
+  try:
+    print("[DATABASE] Reset %s" % sid)
+    storage.pop(sid, None)
+  finally:
+    mutex.release()
 
 def delete(sid, name):
   global storage
+  global mutex
+
   print("[DATABASE] Deleting SID %s - name %s" % (sid, name))
-  if sid not in storage:
-    print("[DATABASE] Error - SID %s does not exist" % sid)
-    return False
-  if name not in storage[sid]:
-    print("[DATABASE] Error - %s does not exist in SID %s" % (name, sid))
-    return False
-  storage[sid].pop(name, None)
+
+  mutex.acquire()
+  try:
+    if sid not in storage:
+      print("[DATABASE] Error - SID %s does not exist" % sid)
+      return False
+    if name not in storage[sid]:
+      print("[DATABASE] Error - %s does not exist in SID %s" % (name, sid))
+      return False
+    storage[sid].pop(name, None)
+  finally:
+    mutex.release()
   print("[DATABASE] Deleted - SID %s - name %s" % (sid, name))
   return True
 
 def persist():
   global storage
-  f = open(FILE, "w")
-  f.write(yaml.safe_dump(storage))
+  global mutex
+
+  mutex.acquire()
+  try:
+    f = open(FILE, "w")
+    f.write(yaml.safe_dump(storage))
+  finally:
+    mutex.release()
+    f.close()
   print("[DATABASE] Persist data to %s" % FILE)
-  f.close()
 
 def load():
   global storage
+  global mutex
+
   f = open(FILE, "r")
-  storage = yaml.safe_load(f.read())
+  mutex.acquire()
+  try:
+    storage = yaml.safe_load(f.read())
+  finally:
+    mutex.release()
+    f.close()
   print("[DATABASE] Load data from %s" % FILE)
-  f.close()
